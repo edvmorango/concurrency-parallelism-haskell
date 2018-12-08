f1232ee1b3b04aa182dc278855af256d86e2a1b3
J. Eduardo V. Morango <edvmorango@gmail.com>
2018-12-08 16:34:44 -0200

    feature: resource sharing example

--- src/MVars.hs
+++ src/MVars.hs
@@ -1,6 +1,7 @@
 module MVars where
 
 import Control.Concurrent
+import qualified Data.Map as M
 
 -- newEmptyMVar :: IO (MVar a)
 -- newMVar :: a -> IO (MVar a)
@@ -70,3 +71,62 @@ mainLogger = do
   _ <- logMessage l "m1"
   _ <- logMessage l "m2"
   logStop l
+
+-- Resource sharing
+type Name = String
+
+type PhoneNumber = String
+
+type PhoneBook = M.Map Name PhoneNumber
+
+newtype PhoneBookState =
+  PhoneBookState (MVar PhoneBook)
+
+newPhoneBook :: IO PhoneBookState
+newPhoneBook = do
+  m <- newMVar M.empty
+  return $ PhoneBookState m
+
+-- $! evaluates before unlock to avoid leaks caused by lazy evaluation
+insertContact :: PhoneBookState -> Name -> PhoneNumber -> IO ()
+insertContact (PhoneBookState s) name phone = do
+  phoneBook <- takeMVar s
+  putMVar s $! M.insert name phone phoneBook
+
+peekPhone :: PhoneBookState -> Name -> IO (Maybe PhoneNumber)
+peekPhone (PhoneBookState s) name = do
+  phoneBook <- takeMVar s
+  _ <- putMVar s phoneBook
+  return $ M.lookup name phoneBook
+
+phoneBookMain :: IO ()
+phoneBookMain = do
+  s <- newPhoneBook
+  sequence_ [insertContact s ("name" ++ show n) (show n) | n <- [1 .. 10000]]
+  peekPhone s "name999" >>= print
+  peekPhone s "some" >>= print
+
+printPhone :: Int -> String -> Maybe String -> IO ()
+printPhone d s Nothing = do
+  _ <- threadDelay (10 ^ 6 * d)
+  putStrLn $ s ++ ": Nothing"
+printPhone d s (Just a) = do
+  _ <- threadDelay (10 ^ 6 * d)
+  putStrLn $ s ++ ": " ++ a
+
+peekInThread :: Int -> String -> PhoneBookState -> IO ()
+peekInThread delay threadName s =
+  sequence_
+    [ (peekPhone s ("name" ++ show n) >>= (printPhone delay threadName))
+    | n <- [1 .. 1000]
+    ]
+
+phoneBookConcurrentMain :: IO ()
+phoneBookConcurrentMain = do
+  s <- newPhoneBook
+  _ <- sequence_ [insertContact s ("name" ++ show n) (show n) | n <- [1 .. 100]]
+  _ <- forkIO $ peekInThread 1 "Thread 1" s
+  _ <- forkIO $ peekInThread 2 "Thread 2" s
+  _ <- peekPhone s "name1"
+  _ <- threadDelay (10 ^ 6 * 10000)
+  putStrLn "Finished"
