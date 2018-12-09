1a4b4f0680aa1b500b5b1a165a230653f25ee2a5
J. Eduardo V. Morango <edvmorango@gmail.com>
2018-12-09 18:02:12 -0200

    feature: monadic example

--- src/NonBlockingIO.hs
+++ src/NonBlockingIO.hs
@@ -4,6 +4,7 @@ module NonBlockingIO where
 
 import Control.Concurrent
 import qualified Data.ByteString.Lazy as B
+import Debug.Trace
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
 
@@ -13,3 +14,17 @@ getUrl url = do
   req <- parseRequest url
   res <- httpLbs req manager
   return $ (B.take 100 . responseBody) res
+
+main :: IO ()
+main = do
+  m1 <- newEmptyMVar
+  m2 <- newEmptyMVar
+  _ <-
+    forkIO $
+    threadDelay (10 ^ 6 * 1) >> (getUrl "https://www.gitlab.org") >>=
+    trace "GitLab" (putMVar m1)
+  _ <-
+    forkIO $ (getUrl "https://www.github.com") >>= trace "GitHub" (putMVar m2)
+  r1 <- trace "m1 evaluated" (takeMVar m1)
+  r2 <- trace "m2 evaluated" (takeMVar m2)
+  putStrLn "Finished"
